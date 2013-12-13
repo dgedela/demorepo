86b732c2d61e5a2a2cdcd080bf956070f51d0e2d
dgedela <saideva.raju@gmail.com>
2013-12-13 12:33:22 +0530

    It is new to DemoRepo

---
+++ MyProject/src/main/java/com/macys/navapp/App.java
@@ -0,0 +1,12 @@
+Package com.nisum.navapp;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("HelloWorld...!");
+    }
+}
